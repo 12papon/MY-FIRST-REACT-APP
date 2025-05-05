7828d87c5947abf4d35e298c01ff08d945ca2509
Rayhan <paponk25@gmail.com>
2025-05-05 16:37:25 +0600

    Code rebice

--- Project-2/src/App.jsx
+++ Project-2/src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import Placement from './components/Placement/Placement'
 import Products from './components/Products/Products'
 import Navbar from './components/Navbar/Navbar'
-import { Outlet } from 'react-router-dom'
 
 function App() {
   const [cart, setCart] = useState([])
@@ -15,18 +14,12 @@ function App() {
   return (
     <div>
       <Navbar></Navbar>
-      <Outlet></Outlet>
         <h1 className='text-2xl font-bold text-lime-700 text-shadow-2xs text-shadow-pink-400'>All Data </h1>
         <div className='border border-amber-300 rounded my-5 p-2 grid grid-cols-2 h-[400px] overflow-scroll'>
           <Products addCart= {addCart}></Products>
           <Placement cart ={cart} ></Placement>
         </div>
-        <div className="card">
-          <h1>heading</h1>
-        </div>
-        <div className="card-2">
-          <h1>heading-1</h1>
-        </div>
+        
     </div>
   )
 }
--- Project-2/src/main.jsx
+++ Project-2/src/main.jsx
@@ -1,38 +1,11 @@
-import { Children, StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import About from './components/About/About.jsx'
-import Contacts from './components/Contact/Contact.jsx'
-import { createBrowserRouter, RouterProvider} from "react-router-dom"
-import Portfolio from './components/Portfolio/Portfolio.jsx'
-import ErrorPage from './components/Errorpage/ErrorPage.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>,
-    errorElement: <ErrorPage></ErrorPage>,
-    children:[
-      {
-        path: '/about',
-        element: <About></About>
-      },
-      {
-        path: '/contacts',
-        element: <Contacts></Contacts>
-      },
-      {
-        path: '/portfolio',
-        element: <Portfolio></Portfolio>
-      }
-    ]
-  }
-  
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <RouterProvider router={router} />
+     <App></App>
   </StrictMode>,
 )
