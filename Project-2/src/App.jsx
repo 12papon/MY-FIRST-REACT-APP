895cf9a194ce1b8d8d8da12038827c41fe32829d
Rayhan <paponk25@gmail.com>
2025-05-05 17:40:22 +0600

    new react project

--- Project-2/src/App.jsx
+++ Project-2/src/App.jsx
@@ -1,33 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import Placement from './components/Placement/Placement'
-import Products from './components/Products/Products'
-import Navbar from './components/Navbar/Navbar'
 
 function App() {
-  const [cart, setCart] = useState([])
-  const addCart = (cartData)=>{
-    const newCart = [...cart, cartData]
-    setCart(newCart)
-  }
-  
+
   return (
     <div>
-      <Navbar></Navbar>
-        <h1 className='text-2xl font-bold text-lime-700 text-shadow-2xs text-shadow-pink-400'>All Data </h1>
-        <div className='border border-amber-300 rounded my-5 p-2 grid grid-cols-2 h-[400px] overflow-scroll'>
-          <Products addCart= {addCart}></Products>
-          <Placement cart ={cart} ></Placement>
-        </div>
-        <div className="card">
-          <h1>heading</h1>
-        </div>
-        <div className="card-2">
-          <h1>heading-1</h1>
-        </div>
-      <div class="card-3">
-        <h1>Heading</h1>
-      </div>
+      <h1>Hallo</h1>
     </div>
   )
 }
